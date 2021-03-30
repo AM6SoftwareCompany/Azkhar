480c4b334a73a9d565609ab5a376484c1070b8c4
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-03-31 00:59:59 +0200

    made a starter bot

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import time
 import webbrowser
 
+import pyautogui
+
 webbrowser.open_new('https://business.facebook.com/creatorstudio/home')
+time.sleep(10)
+pyautogui.moveTo(950, 300, duration=1)
+pyautogui.click()
+pyautogui.PAUSE = 5
+pyautogui.moveTo(900, 200, duration=1)
+pyautogui.click()
+pyautogui.write('apocryphon')
+pyautogui.moveTo(970, 270, duration=1)
+pyautogui.click()
+pyautogui.moveTo(1000, 500, duration=1)
+pyautogui.click()
+pyautogui.moveTo(150, 400, duration=1)
+pyautogui.click()
+pyautogui.write('Bot Test1!')
+pyautogui.moveTo(250, 700, duration=1)
+pyautogui.click()
